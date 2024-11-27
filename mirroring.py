b1cceec2a7a2b262770e99fcfb3e1f2d21680b5a
Naufal123-j <114628212+Naufal123-j@users.noreply.github.com>
2024-11-27 17:53:17 +0700

    Add files via upload

--- mirroring.py
+++ mirroring.py
@@ -2,30 +2,29 @@ import numpy as np
 import imageio as img 
 import matplotlib.pyplot as plt 
 
+# Membaca gambar
 path = "C:\\gambar\\bunga.jpg"
 image = img.imread(path)
 
 height, width = image.shape[:2]
-horizontal = np.zeros_like(image)
-vertical = np.zeros_like(image)
 
+# Buat array kosong untuk hasil mirroring
+mirrored_image = np.zeros_like(image)
+
+# Proses mirroring vertikal dan horizontal secara bersamaan
 for y in range(height):
     for x in range(width):
-        horizontal[y, x] = image[y, width - 1 - x]
+        mirrored_image[y, x] = image[height - 1 - y, width - 1 - x]
 
-for y in range(height):
-    for x in range(width):
-        vertical[y,x] = image[height - 1-y,x]
-
+# Menampilkan gambar asli dan hasil mirroring
 plt.figure(figsize=(10, 5))
 
-plt.subplot(1,3,1)
+plt.subplot(1, 2, 1)
+plt.title("Original Image")
 plt.imshow(image)
 
-plt.subplot(1,3,2)
-plt.imshow(horizontal)
-
-plt.subplot(1,3,3)
-plt.imshow(vertical)
+plt.subplot(1, 2, 2)
+plt.title("Mirrored Image (Vertical & Horizontal)")
+plt.imshow(mirrored_image)
 
 plt.show()
